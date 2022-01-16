b3148a683be6a1203a2e6bfc1ecd052f8945b5b9
SantiiPosada <santi.posada.contacto@gmail.com>
2022-01-15 19:40:40 -0500

    Componentes listo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
+import { VideojuegosComponent } from './components/videojuegos/videojuegos.component';
+import { VideojuegoComponent } from './components/videojuego/videojuego.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BuscadorComponent,
+    VideojuegosComponent,
+    VideojuegoComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
